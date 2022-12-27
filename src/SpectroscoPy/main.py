7d7e8413ab77377021a832e60c0799442766ef81
James <james.sturgis@univ-amu.fr>
2022-12-27 18:49:03 +0100

    Create a messages module and a main...

---
+++ src/SpectroscoPy/main.py
@@ -0,0 +1,9 @@
+""" Main file for SpectrscoPy program
+
+For the moment it just displays a licencing message, but it is a start.
+"""
+
+import messages
+
+if __name__ == '__main__':
+    print( messages.ABOUT )
---
+++ src/SpectroscoPy/messages.py
@@ -0,0 +1,9 @@
+""" Module containing the different messages given by the program
+
+"""
+
+ABOUT = ''.join(("                   SpectroscoPy\n",
+                 "       Copyright (C) 2023 James N. Sturgis \n",
+                 "This program comes with ABSOLUTELY NO WARRANTY;\n",
+                 "This is free software, and you are welcome to redistribute\n",
+                 "it under certain conditions; see the GPL V3.0 Licence."))
