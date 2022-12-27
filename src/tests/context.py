644f43e0ac866aa7563ec3cac38a66e045d9e318
James <james.sturgis@univ-amu.fr>
2022-12-27 17:20:00 +0100

    Finish the last one

--- src/tests/context.py
+++ src/tests/context.py
@@ -10,6 +10,6 @@ https://docs.python-guide.org/writing/structure/
 import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),
-    '../SpectroscoPy')))
+                '../SpectroscoPy')))
 
 import spectra
