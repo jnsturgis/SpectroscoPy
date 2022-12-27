c3d8cc66f5057178d7b4ef33345bc0723627df2c
James <james.sturgis@univ-amu.fr>
2022-12-27 17:19:05 +0100

    GPLv3.0 and test/context

---
+++ src/tests/context.py
@@ -0,0 +1,15 @@
+
+""" File to allow context independent execution of tests
+
+To use in individual test files start with the line:
+'from .context import spectra'
+This should work independent of the installation method see:
+https://docs.python-guide.org/writing/structure/
+"""
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),
+    '../SpectroscoPy')))
+
+import spectra
