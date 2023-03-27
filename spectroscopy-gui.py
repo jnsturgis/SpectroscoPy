462afeda252c65c7c9f21c2d88d3a95fd3224def
James <james.sturgis@univ-amu.fr>
2023-03-27 23:09:46 +0200

    Remove ignored files

--- spectroscopy-gui.py
+++ spectroscopy-gui.py
@@ -23,65 +23,79 @@ import numpy as np
 import spectroscopy as spec
 import spectroscopy.gui as gui
 
-def main():
+class App(tk.Tk):
+
+    def __init__(self):
+        tk.Tk.__init__(self)
 # Chat GPT example to use Matplotlib
 
 # Create a Tkinter window
-    root = tk.Tk()
-    root.title("Spectroscopy")
-    root.geometry("400x150")
+        self.title("Spectroscopy")
+        self.geometry("400x150")
 
-    # Chat GPT for menubars
-    menu_bar = tk.Menu(root)
-    file_menu = tk.Menu(menu_bar, tearoff=0)
-    file_menu.add_command(label="Open", command=open_file)
-    file_menu.add_command(label="Open Recent", command=save_file)
-    file_menu.add_command(label="Import", command=save_file)
-    file_menu.add_command(label="Save", command=save_file)
-    file_menu.add_command(label="Save As", command=save_file)
-    file_menu.add_command(label="Export", command=save_file)
-    file_menu.add_command(label="Plot", command=save_file)
-    file_menu.add_command(label="Add to Plot", command=save_file)
-    file_menu.add_command(label="Close", command=save_file)
-    file_menu.add_command(label="Close All", command=save_file)
-    file_menu.add_command(label="Exit", command=root.destroy )
-    menu_bar.add_cascade(label="File", menu=file_menu)
+    # Set up the menus for the Application
+        menu_bar = tk.Menu(self)
+        file_menu = tk.Menu(menu_bar, tearoff=0)
+        file_menu.add_command(label="Open", command=open_file)
+        file_menu.add_command(label="Open Recent", command=save_file)
+        file_menu.add_command(label="Import", command=save_file)
+        file_menu.add_separator()
+        file_menu.add_command(label="Save", command=save_file)
+        file_menu.add_command(label="Save As", command=save_file)
+        file_menu.add_command(label="Export", command=save_file)
+        file_menu.add_separator()
+        file_menu.add_command(label="Plot", command=save_file)
+        file_menu.add_command(label="Add to Plot", command=save_file)
+        file_menu.add_separator()
+        file_menu.add_command(label="Close", command=save_file)
+        file_menu.add_command(label="Close All", command=save_file)
+        file_menu.add_command(label="Exit", command=self.quit )
+        menu_bar.add_cascade(label="File", menu=file_menu)
 
-    edit_menu = tk.Menu(menu_bar, tearoff=0)
-    edit_menu.add_command(label="Undo", command=save_file)
-    edit_menu.add_command(label="Redo", command=save_file)
-    edit_menu.add_command(label="Edit history", command=edit_data)
-    edit_menu.add_command(label="Edit acquisition", command=save_file)
-    edit_menu.add_command(label="Edit metadata", command=save_file)
-    edit_menu.add_command(label="Convert x", command=save_file)
-    edit_menu.add_command(label="Convert y", command=save_file)
-    edit_menu.add_command(label="Shift x", command=save_file)
-    edit_menu.add_command(label="Shift y", command=save_file)
-    edit_menu.add_command(label="Resample", command=save_file)
-    menu_bar.add_cascade(label="Edit", menu=edit_menu)
+        edit_menu = tk.Menu(menu_bar, tearoff=0)
+        edit_menu.add_command(label="Undo", command=save_file)
+        edit_menu.add_command(label="Redo", command=save_file)
+        edit_menu.add_separator()
+        edit_menu.add_command(label="Edit history", command=edit_data)
+        edit_menu.add_command(label="Edit acquisition", command=save_file)
+        edit_menu.add_command(label="Edit metadata", command=save_file)
+        edit_menu.add_separator()
+        edit_menu.add_command(label="Convert x", command=save_file)
+        edit_menu.add_command(label="Convert y", command=save_file)
+        edit_menu.add_separator()
+        edit_menu.add_command(label="Shift x", command=save_file)
+        edit_menu.add_command(label="Shift y", command=save_file)
+        edit_menu.add_separator()
+        edit_menu.add_command(label="Resample", command=save_file)
+        menu_bar.add_cascade(label="Edit", menu=edit_menu)
 
-    treat_menu = tk.Menu(menu_bar, tearoff=0)
-    treat_menu.add_command(label="Smooth", command=save_file)
-    treat_menu.add_command(label="Differentiate", command=save_file)
-    treat_menu.add_command(label="Integrate", command=save_file)
-    treat_menu.add_command(label="Baseline subtract", command=save_file)
-    treat_menu.add_command(label="Spectrum calculator", command=save_file)
-    treat_menu.add_command(label="Sharpen peaks", command=save_file)
-    treat_menu.add_command(label="Scattering correction", command=save_file)
-    treat_menu.add_command(label="ATR correction", command=save_file)
-    menu_bar.add_cascade(label="Treatment", menu=treat_menu)
+        treat_menu = tk.Menu(menu_bar, tearoff=0)
+        treat_menu.add_command(label="Smooth", command=save_file)
+        treat_menu.add_command(label="Differentiate", command=save_file)
+        treat_menu.add_command(label="Integrate", command=save_file)
+        treat_menu.add_command(label="Baseline subtract", command=save_file)
+        treat_menu.add_command(label="Spectrum calculator", command=save_file)
+        treat_menu.add_command(label="Sharpen peaks", command=save_file)
+        treat_menu.add_command(label="Scattering correction", command=save_file)
+        treat_menu.add_command(label="ATR correction", command=save_file)
+        menu_bar.add_cascade(label="Treatment", menu=treat_menu)
 
-    analysis_menu = tk.Menu(menu_bar, tearoff=0)
-    analysis_menu.add_command(label="Peak fitting", command=save_file)
-    analysis_menu.add_command(label="Identification", command=save_file)
-    analysis_menu.add_command(label="Spectrum fitting", command=save_file)
-    menu_bar.add_cascade(label="Analysis", menu=analysis_menu)
+        analysis_menu = tk.Menu(menu_bar, tearoff=0)
+        analysis_menu.add_command(label="Peak fitting", command=save_file)
+        analysis_menu.add_command(label="Identification", command=save_file)
+        analysis_menu.add_command(label="Spectrum fitting", command=save_file)
+        menu_bar.add_cascade(label="Analysis", menu=analysis_menu)
 
-    root.config(menu=menu_bar)
+        help_menu = tk.Menu(menu_bar, tearoff=0)
+        help_menu.add_command(label="About", command=save_file)
+        help_menu.add_command(label="Help", command=save_file)
+        menu_bar.add_cascade(label="Help", menu=help_menu)
 
-# Run the GUI
-    root.mainloop()
+        self.config(menu=menu_bar)
 
+    def quit(self, event):
+        print("quitting...")
+        sys.exit(0)
 
 def open_file():
 
@@ -106,4 +120,5 @@ def save_file():
     pass
 
 if __name__ == '__main__':
-    main()
+    app = App()
+    app.mainloop()
